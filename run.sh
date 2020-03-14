f4afc5e1dc5523892be1eee697a0c651212f9238
qing.liu <qing.liu@ihandysoft.com>
2020-03-14 19:53:25 +0800

    run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBILE_DEVICES=1 python yolov3_deepsort.py --ignore_display --save_path demo/output/skateboarding.mp4 demo/skateboarding.mp4
+CUDA_VISIBLE_DEVICES=1 python yolov3_deepsort.py --ignore_display --save_path demo/output/skateboarding.mp4 demo/skateboarding.mp4
