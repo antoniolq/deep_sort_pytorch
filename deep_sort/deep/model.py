8e385ea89a75ee8eaa913358be836b8a7bb4377f
qing.liu <qing.liu@ihandysoft.com>
2020-03-21 17:07:41 +0800

    res2net

--- deep_sort/deep/model.py
+++ deep_sort/deep/model.py
@@ -148,7 +148,7 @@ class Net(nn.Module):
         # 256 1 1 
         self.reid = reid
         self.classifier = nn.Sequential(
-            nn.Linear(131072, 256),
+            nn.Linear(1, 256),
             nn.BatchNorm1d(256),
             nn.ReLU(inplace=True),
             nn.Dropout(),
