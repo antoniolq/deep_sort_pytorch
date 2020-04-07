e34f3e52bf250462304f12bd5608717234e016d1
qing.liu <qing.liu@ihandysoft.com>
2020-04-07 18:58:14 +0800

    baseline

---
+++ mot16.py
@@ -0,0 +1,81 @@
+import os
+import cv2
+import time
+import argparse
+import torch
+import numpy as np
+
+from detector import build_detector
+from deep_sort import build_tracker
+from utils.draw import draw_boxes
+from utils.parser import get_config
+from skimage import io
+
+class imageTracker(object):
+    def __init__(self, cfg, args):
+        self.cfg = cfg
+        self.args = args
+        use_cuda = args.use_cuda and torch.cuda.is_available()
+        if not use_cuda:
+            raise UserWarning("Running in cpu mode!")
+
+        self.dir = "/mnt/Disk1/qingl/data/MOT16/train/MOT16-02/img1/"
+        self.detector = build_detector(cfg, use_cuda=use_cuda)
+        self.deepsort = build_tracker(cfg, use_cuda=use_cuda)
+        self.class_names = self.detector.class_names
+
+
+    def __enter__(self):
+        return self
+
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type:
+            print(exc_type, exc_value, exc_traceback)
+
+
+    def run(self):
+        images = os.listdir(self.dir)
+        idx_frame = 0
+        sample = list(images)
+        while idx_frame <= len(sample):
+            tmp = self.dir + images[0]
+            img = io.imread(tmp)
+            start = time.time()
+            im = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            bbox_xywh, cls_conf, cls_ids = self.detector(im)
+            if bbox_xywh is not None:
+                # select person class
+                mask = cls_ids==0
+
+                bbox_xywh = bbox_xywh[mask]
+                bbox_xywh[:,3:] *= 1.2 # bbox dilation just in case bbox too small
+                cls_conf = cls_conf[mask]
+
+                # do tracking
+                results = self.deepsort.update(bbox_xywh, cls_conf, im)
+                f = open("/home/qingl/antonio/mot16/MOT16-02.txt", 'a')
+                for row in results:
+                    print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1' % (idx_frame,
+                        row[0], row[1], row[2], row[3], row[4]), file=f)
+            idx_frame += 1
+            end = time.time()
+            print("time: {:.03f}s, fps: {:.03f}".format(end-start, 1/(end-start)))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_detection", type=str, default="./configs/yolov3.yaml")
+    parser.add_argument("--config_deepsort", type=str, default="./configs/deep_sort.yaml")
+    parser.add_argument("--cpu", dest="use_cuda", action="store_false", default=True)
+    return parser.parse_args()
+
+
+if __name__=="__main__":
+    args = parse_args()
+    cfg = get_config()
+    cfg.merge_from_file(args.config_detection)
+    cfg.merge_from_file(args.config_deepsort)
+
+    with imageTracker(cfg, args) as img_trk:
+        img_trk.run()
