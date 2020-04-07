22fc7611f84aa6b74b8c31541b9a97b43ddefd89
qing.liu <qing.liu@ihandysoft.com>
2020-04-07 13:16:11 +0800

    test

--- mot16.py
+++ mot16.py
@@ -11,7 +11,7 @@ from utils.draw import draw_boxes
 from utils.parser import get_config
 
 
-class VideoTracker(object):
+class imageTracker(object):
     def __init__(self, cfg, args):
         self.cfg = cfg
         self.args = args
@@ -19,44 +19,18 @@ class VideoTracker(object):
         if not use_cuda:
             raise UserWarning("Running in cpu mode!")
 
-        if args.display:
-            cv2.namedWindow("test", cv2.WINDOW_NORMAL)
-            cv2.resizeWindow("test", args.display_width, args.display_height)
-
-        self.vdo = cv2.VideoCapture()
+        self.dir = "/mnt/Disk1/qingl/data/MOT16/train/MOT16-02/img1/"
         self.detector = build_detector(cfg, use_cuda=use_cuda)
         self.deepsort = build_tracker(cfg, use_cuda=use_cuda)
         self.class_names = self.detector.class_names
 
-
-    def __enter__(self):
-        assert os.path.isfile(self.args.VIDEO_PATH), "Error: path error"
-        self.vdo.open(self.args.VIDEO_PATH)
-        self.im_width = int(self.vdo.get(cv2.CAP_PROP_FRAME_WIDTH))
-        self.im_height = int(self.vdo.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-        if self.args.save_path:
-            fourcc =  cv2.VideoWriter_fourcc(*'MJPG')
-            self.writer = cv2.VideoWriter(self.args.save_path, fourcc, 20, (self.im_width,self.im_height))
-
-        assert self.vdo.isOpened()
-        return self
-
-    
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        if exc_type:
-            print(exc_type, exc_value, exc_traceback)
-        
-
     def run(self):
+        images = os.listdir(self.dir)
         idx_frame = 0
-        while self.vdo.grab(): 
+        while idx_frame >= len(images):
             idx_frame += 1
-            if idx_frame % self.args.frame_interval:
-                continue
-
             start = time.time()
-            _, ori_im = self.vdo.retrieve()
+            _, ori_im = images[idx_frame]
             im = cv2.cvtColor(ori_im, cv2.COLOR_BGR2RGB)
 
             # do detection
@@ -69,7 +43,6 @@ class VideoTracker(object):
                 bbox_xywh[:,3:] *= 1.2 # bbox dilation just in case bbox too small
                 cls_conf = cls_conf[mask]
 
-                # do tracking
                 outputs = self.deepsort.update(bbox_xywh, cls_conf, im)
                 f = open("/home/qingl/antonio/mot16/test.txt", 'a')
                 for row in outputs:
@@ -79,24 +52,12 @@ class VideoTracker(object):
             end = time.time()
             print("time: {:.03f}s, fps: {:.03f}".format(end-start, 1/(end-start)))
 
-            # if self.args.display:
-            #     cv2.imshow("test", ori_im)
-            #     cv2.waitKey(1)
-
-            # if self.args.save_path:
-            #     self.writer.write(ori_im)
-            
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("VIDEO_PATH", type=str)
     parser.add_argument("--config_detection", type=str, default="./configs/yolov3.yaml")
     parser.add_argument("--config_deepsort", type=str, default="./configs/deep_sort.yaml")
-    parser.add_argument("--ignore_display", dest="display", action="store_false", default=True)
     parser.add_argument("--frame_interval", type=int, default=1)
-    parser.add_argument("--display_width", type=int, default=800)
-    parser.add_argument("--display_height", type=int, default=600)
-    parser.add_argument("--save_path", type=str, default="./demo/demo.avi")
     parser.add_argument("--cpu", dest="use_cuda", action="store_false", default=True)
     return parser.parse_args()
 
@@ -107,5 +68,5 @@ if __name__=="__main__":
     cfg.merge_from_file(args.config_detection)
     cfg.merge_from_file(args.config_deepsort)
 
-    with VideoTracker(cfg, args) as vdo_trk:
-        vdo_trk.run()
+    with imageTracker(cfg, args) as img_trk:
+        img_trk.run()
