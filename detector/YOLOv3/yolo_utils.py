ee0390cf5bace0fa02954d19fc31785e42d6275f
qing.liu <qing.liu@ihandysoft.com>
2020-04-12 10:49:06 +0800

    test

--- detector/YOLOv3/yolo_utils.py
+++ detector/YOLOv3/yolo_utils.py
@@ -90,6 +90,7 @@ def post_process(boxes, num_classes, conf_thresh=0.01, nms_thresh=0.45, obj_thre
             # keep = boxes_nms(masked_boxes[:,:4], masked_boxes[:,5], nms_thresh)
             # np.save("test/keep", keep.cpu().numpy())
             nmsed_boxes = nms(masked_boxes, nms_thresh)
+            np.save("test/nmsed_boxes2", nmsed_boxes.cpu().numpy())
             # nmsed_boxes = masked_boxes[keep, :]
             # np.save("test/nmsed_boxes", nmsed_boxes.cpu().numpy())
             processed_boxes.append(nmsed_boxes)
