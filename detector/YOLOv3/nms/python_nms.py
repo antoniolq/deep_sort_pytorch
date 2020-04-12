48248ed979ce264dc8b4a8c469b85ed40f96e922
qing.liu <qing.liu@ihandysoft.com>
2020-04-12 19:49:02 +0800

    test

--- detector/YOLOv3/nms/python_nms.py
+++ detector/YOLOv3/nms/python_nms.py
@@ -2,6 +2,48 @@ import torch
 import numpy as np
 
 
+def Diou(bboxes1, bboxes2):
+    rows = bboxes1.shape[0]
+    cols = bboxes2.shape[0]
+    dious = torch.zeros((rows, cols))
+    if rows * cols == 0:  #
+        return dious
+    exchange = False
+    if bboxes1.shape[0] > bboxes2.shape[0]:
+        bboxes1, bboxes2 = bboxes2, bboxes1
+        dious = torch.zeros((cols, rows))
+        exchange = True
+    # #xmin,ymin,xmax,ymax->[:,0],[:,1],[:,2],[:,3]
+    w1 = bboxes1[:, 2] - bboxes1[:, 0]
+    h1 = bboxes1[:, 3] - bboxes1[:, 1]
+    w2 = bboxes2[:, 2] - bboxes2[:, 0]
+    h2 = bboxes2[:, 3] - bboxes2[:, 1]
+
+    area1 = w1 * h1
+    area2 = w2 * h2
+
+    center_x1 = (bboxes1[:, 2] + bboxes1[:, 0]) / 2
+    center_y1 = (bboxes1[:, 3] + bboxes1[:, 1]) / 2
+    center_x2 = (bboxes2[:, 2] + bboxes2[:, 0]) / 2
+    center_y2 = (bboxes2[:, 3] + bboxes2[:, 1]) / 2
+
+    inter_max_xy = torch.min(bboxes1[:, 2:], bboxes2[:, 2:])
+    inter_min_xy = torch.max(bboxes1[:, :2], bboxes2[:, :2])
+    out_max_xy = torch.max(bboxes1[:, 2:], bboxes2[:, 2:])
+    out_min_xy = torch.min(bboxes1[:, :2], bboxes2[:, :2])
+
+    inter = torch.clamp((inter_max_xy - inter_min_xy), min=0)
+    inter_area = inter[:, 0] * inter[:, 1]
+    inter_diag = (center_x2 - center_x1) ** 2 + (center_y2 - center_y1) ** 2
+    outer = torch.clamp((out_max_xy - out_min_xy), min=0)
+    outer_diag = (outer[:, 0] ** 2) + (outer[:, 1] ** 2)
+    union = area1 + area2 - inter_area
+    dious = inter_area / union - (inter_diag) / outer_diag
+    dious = torch.clamp(dious, min=-1.0, max=1.0)
+    if exchange:
+        dious = dious.T
+    return dious
+
 def python_nms(boxes, scores, nms_thresh):
     """ Performs non-maximum suppression using numpy
         Args:
@@ -27,7 +69,7 @@ def python_nms(boxes, scores, nms_thresh):
     areas = (x2 - x1) * (y2 - y1)
     order = np.argsort(scores)[::-1]
     num_detections = boxes.shape[0]
-    suppressed = np.zeros((num_detections,), dtype=np.bool)
+    suppressed = np.zeros((num_detections,), dtype=np.float)
     for _i in range(num_detections):
         i = order[_i]
         if suppressed[i]:
@@ -50,11 +92,18 @@ def python_nms(boxes, scores, nms_thresh):
             w = max(0, xx2 - xx1)
             h = max(0, yy2 - yy1)
 
+            x_center = xx1 + (xx2 - xx1) / 2
+            y_center = yy1 + (yy2 - yy1) / 2
+            x1_center = x1[j] + (x2[j] - x1[j]) / 2
+            y1_center = y1[j] + (y2[j] - y1[j]) / 2
+            inter_diag = (x1_center - x_center) ** 2 + (y1_center - y_center) ** 2
+            outer_diag = (x2[j] - ix1) ** 2 + (y2[j] - iy1) ** 2
             inter = w * h
             ovr = inter / (iarea + areas[j] - inter)
-            if ovr >= nms_thresh:
+            dious = ovr - inter_diag / outer_diag
+            if dious >= nms_thresh:
                 suppressed[j] = np.exp(-(ovr * ovr) / 0.5) * suppressed[j]
     keep = np.nonzero(suppressed == 0)[0]
     keep = torch.from_numpy(keep).to(origin_device)
-    print("yes")
+    # print("yes")
     return keep
