467d25d67c56c04427b33511a8015bc61560a1bc
qing.liu <qing.liu@ihandysoft.com>
2020-04-12 12:52:37 +0800

    test

--- detector/YOLOv3/yolo_utils.py
+++ detector/YOLOv3/yolo_utils.py
@@ -89,8 +89,8 @@ def post_process(boxes, num_classes, conf_thresh=0.01, nms_thresh=0.45, obj_thre
             mask = (boxes[batch_id, :, -1] == cls_id) * (boxes[batch_id, :, 4] > obj_thresh)
             masked_boxes = boxes[batch_id, mask]
 
-            keep = boxes_nms(masked_boxes[:, :4], masked_boxes[:, 5], nms_thresh)
-            # keep = nms(masked_boxes, nms_thresh)
+            # keep = boxes_nms(masked_boxes[:, :4], masked_boxes[:, 5], nms_thresh)
+            keep = nms(masked_boxes, nms_thresh)
             keep = torch.tensor(keep, dtype=int)
             print(keep)
             nmsed_boxes = masked_boxes[keep, :]
