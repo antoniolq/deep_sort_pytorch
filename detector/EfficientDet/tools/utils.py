d72a811fbc4b381d2c89e27f53f775b7ec1195f0
qing.liu <qing.liu@ihandysoft.com>
2020-04-20 00:08:33 +0800

    id

--- detector/EfficientDet/tools/utils.py
+++ detector/EfficientDet/tools/utils.py
@@ -66,6 +66,7 @@ def aspectaware_resize_padding(image, width, height, interpolation=None, means=N
 def preprocess(*image_path, max_size=512, mean=(0.406, 0.456, 0.485), std=(0.225, 0.224, 0.229)):
     # ori_imgs = cv2.imread(ori_img)
     ori_imgs = [cv2.imread(img_path) for img_path in image_path]
+    print(type(ori_imgs))
     # cv2.imshow(ori_imgs)
     normalized_imgs = [(img / 255 - mean) / std for img in ori_imgs]
     imgs_meta = [aspectaware_resize_padding(img[..., ::-1], max_size, max_size,
