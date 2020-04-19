c3b51f77824e973ad91e17381540b7d646db48ab
qing.liu <qing.liu@ihandysoft.com>
2020-04-19 17:05:03 +0800

    test

--- detector/EfficientDet/tools/utils.py
+++ detector/EfficientDet/tools/utils.py
@@ -11,7 +11,7 @@ from torchvision.ops import nms
 from typing import Union
 import uuid
 
-from utils.sync_batchnorm import SynchronizedBatchNorm2d
+from tools.sync_batchnorm import SynchronizedBatchNorm2d
 
 
 def invert_affine(metas: Union[float, list, tuple], preds):
