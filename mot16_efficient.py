ebd2c9f798aa2437e1a0432f80695f587ad2c03b
qing.liu <qing.liu@ihandysoft.com>
2020-05-25 23:22:34 +0800

    fs

--- mot16_efficient.py
+++ mot16_efficient.py
@@ -12,7 +12,7 @@ from utils.parser import get_config
 from skimage import io
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 class imageTracker(object):
     def __init__(self, cfg, args, name):
